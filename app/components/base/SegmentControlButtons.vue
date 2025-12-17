7adcbbed00b71bb82775e0821a375bf6b6bf1696
Guille <guillevilatte@gmail.com>
2025-12-17 20:24:41 +0100

    wip

--- app/components/base/SegmentControlButtons.vue
+++ app/components/base/SegmentControlButtons.vue
@@ -24,7 +24,7 @@ const handleSelect = (sel: string) => {
         :key="element"
         :class="
           selected === element
-            ? 'bg-orange-300 text-white hover:text-white hover:bg-orange-300'
+            ? 'bg-orange-400 text-white hover:text-white hover:bg-orange-300'
             : ''
         "
         class="text-lg px-6 py-2 cursor-pointer hover:bg-gray-200 text-gray-900 border-r border-gray-300 transition duration-150 ease-in-out"
--- app/components/form/StepNine.vue
+++ app/components/form/StepNine.vue
@@ -8,6 +8,18 @@ type changeStepEmits = {
 const emit = defineEmits<changeStepEmits>()
 
 const form = useFormStore()
+const emailRe =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+const numberRe = /^(?:\+34|0034)?\s?[6789]\d{2}(?:\s?\d{3}){2}$/
+
+const errorEmail = ref(false)
+const errorNumber = ref(false)
+
+const validate = (from: string) => {
+  if (from === 'email') errorEmail.value = !emailRe.test(form.form.human.email)
+  else errorNumber.value = !numberRe.test(form.form.human.number)
+  console.log(errorEmail.value, errorNumber.value)
+}
 </script>
 
 <template>
@@ -25,19 +37,25 @@ const form = useFormStore()
         v-model="form.form.human.email"
         placeholder="tumejorcorreo@gmail.com"
         class="bg-gray-100 py-3 px-6 mb-1 w-full border border-gray-300 rounded-3xl focus:outline-2 focus:outline-orange-400"
+        @input="validate('email')"
       />
+      <p v-if="errorEmail" class="text-red-500 text-sm">
+        El formato de correo electrónico no es correcto.
+      </p>
       <input
         type="text"
         id="phone"
         v-model="form.form.human.number"
         placeholder="611 222 333"
         class="bg-gray-100 py-3 px-6 mb-1 w-full border border-gray-300 rounded-3xl focus:outline-2 focus:outline-orange-400"
+        @input="validate('number')"
       />
+      <p v-if="errorNumber" class="text-red-500 text-sm">El formato de teléfono no es correcto.</p>
     </div>
     <div class="h-[50px]">
       <ButtonComponent
         :text="'Ver el menú de ' + form.form.name"
-        :disabled="!form.form.human.number || !form.form.human.email"
+        :disabled="!form.form.human.email || errorEmail || !form.form.human.number || errorNumber"
         @click="emit('changeStep')"
       />
     </div>
